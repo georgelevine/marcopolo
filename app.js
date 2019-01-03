a086a7f4b186c7589a0322fecd27b06a21ead216
George Levine <levineg@student.mville.edu>
2019-01-03 01:31:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-/**
- 
-November 9th, 2018 
-
- * Development Stage: Alpha  
- * Script Name and Description: app.js is our implementation of a proximity-based feedback mechanism.
- * Developed for PHY.4000-Electronics Prototyping at Manhattanville College
-
- * Developers:  Brandon Neff, Zach Rowell, Alegria Haro, George Levine
- 
- * Supervisor:  Austin Purves, Ph.D.
-				Assistant Professor, Chair
-				Physics Department
-				Manhattanville College
-				
- * Forked Source From (GitHub): https://github.com/cruepprich/gateOpener/blob/master/gate.js
- * License: GNU General Public License, version 3 (GPLv3)
-
-   Pi Bluetooth Library
-   -------------------- 
- * The noble library is used to read bluetooth signals.
- * (https://github.com/sandeepmistry/noble)
- *
- * Pi GPIO Library
-   ---------------
- * The onoff library is used to access the GPIO ports on the Raspberry Pi.
- * https://github.com/fivdi/onoff
- 
- **/
-
-
-var RSSI_THRESHOLD    = -91
-var EXIT_GRACE_PERIOD = 3000; // milliseconds
-
-=======
 /*
 
  Developers:  George Levine, Brandon Neff, Zach Rowell
